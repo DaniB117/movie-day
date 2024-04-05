79c4bed3a73880eccd928bc8023740dc4d08f19b
DaniB117 <skatedany56@hotmail.com>
2024-04-04 18:08:52 -0600

    Update routes

--- server/routes.js
+++ server/routes.js
@@ -4,7 +4,7 @@ import { MovieController } from './controllers/movieController.js'
 export const moviesRouter = Router()
 
 moviesRouter.get('/api/providers', MovieController.getProviders)
-moviesRouter.post('/api/movies', MovieController.getMovies)
+moviesRouter.get('/api/movies', MovieController.getMovies)
 moviesRouter.get('/api/movie/:id', MovieController.getMovieById )
 
 moviesRouter.get('/', MovieController.index)
